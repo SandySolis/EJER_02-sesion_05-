e279a5e446be3b7f715a193b7fcb0ce2762fd930
Sandy Solis <sandysolis0305@gmail.com>
2023-09-16 23:18:04 -0500

    Agregar archivos de proyecto.

---
+++ EJER_02(sesion_05)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJER_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la base: ");
+            int factor1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el exponente: ");
+            int factor2 = int.Parse(Console.ReadLine());
+            int resultado = 1;
+            for (int i = 0; i < factor2; i++)
+            {
+                resultado = factor1 * resultado;
+            }
+            Console.WriteLine("La potencia es: " + resultado +".");
+            Console.ReadKey();
+        }
+    }
+}
